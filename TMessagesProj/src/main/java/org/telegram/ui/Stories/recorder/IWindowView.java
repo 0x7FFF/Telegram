b470e0ccb9a85ee5a2b38da1c531954bf54b386c
0x7fff <gvozdevik@gmail.com>
2024-12-22 11:31:30 +0300

    Story Camera port

---
+++ TMessagesProj/src/main/java/org/telegram/ui/Stories/recorder/IWindowView.java
@@ -0,0 +1,19 @@
+package org.telegram.ui.Stories.recorder;
+
+import android.graphics.Bitmap;
+import android.view.View;
+import android.view.ViewGroup;
+
+public interface IWindowView {
+
+    void addView(View child, ViewGroup.LayoutParams layoutParams);
+    void addView(View child);
+    void invalidate();
+    void drawBlurBitmap(Bitmap bitmap, float amount);
+    int getPaddingUnderContainer();
+    int getMeasuredHeight();int getMeasuredWidth();
+
+    int getBottomPadding2();
+    void removeView(View view);
+    View asView();
+}
